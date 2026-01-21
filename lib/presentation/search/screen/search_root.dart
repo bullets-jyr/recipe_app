7da79ff46b893b12b3c5b0de25dbcbb4b8bb8ca4
bullets-jyr <jy.ryu.jiyoung@gmail.com>
2026-01-21 14:25:16 +0900

    feat: Save and load search results locally

--- lib/presentation/search/screen/search_root.dart
+++ lib/presentation/search/screen/search_root.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_recipe_app_course/data/data_source/local/local_recent_search_recipe_data_source_impl.dart';
+import 'package:flutter_recipe_app_course/data/data_source/local/default_local_storage.dart';
+import 'package:flutter_recipe_app_course/data/data_source/local_storage.dart';
 import 'package:flutter_recipe_app_course/data/data_source/remote/remote_recipe_data_source_impl.dart';
 import 'package:flutter_recipe_app_course/data/repository/mock_recent_search_recipe_repository_impl.dart';
 import 'package:flutter_recipe_app_course/data/repository/mock_recipe_repository_impl.dart';
@@ -7,8 +8,10 @@ import 'package:flutter_recipe_app_course/domain/use_case/search_recipes_use_cas
 import 'package:flutter_recipe_app_course/presentation/search/screen/search_screen.dart';
 import 'package:flutter_recipe_app_course/presentation/search/search_view_model.dart';
 
+final LocalStorage _localStorage = DefaultLocalStorage();
+
 final _recentSearchRecipeRepository = MockRecentSearchRecipeRepositoryImpl(
-  recipeDataSource: LocalRecentSearchRecipeDataSourceImpl(),
+  localStorage: _localStorage,
 );
 
 class SearchRoot extends StatelessWidget {
@@ -22,6 +25,7 @@ class SearchRoot extends StatelessWidget {
         recipeRepository: MockRecipeRepositoryImpl(
           recipeDataSource: RemoteRecipeDataSourceImpl(),
         ),
+        localStorage: _localStorage,
       ),
     );
 
@@ -30,10 +34,6 @@ class SearchRoot extends StatelessWidget {
       builder: (context, widget) {
         return SearchScreen(
           state: viewModel.state,
-          // onChanged: (query) {
-          //   viewModel.searchRecipes(query);
-          // },
-          // onChanged: (query) => viewModel.searchRecipes(query),
           onChanged: viewModel.searchRecipes,
         );
       },
