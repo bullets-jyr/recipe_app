8e0053d7d5b40d2f8dab566472e8ef75b435c731
bullets-jyr <jy.ryu.jiyoung@gmail.com>
2026-01-21 16:09:00 +0900

    feat: Add filter UI and functionality

--- lib/presentation/search/screen/search_root.dart
+++ lib/presentation/search/screen/search_root.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_recipe_app_course/core/di/di_setup.dart';
+import 'package:flutter_recipe_app_course/core/presentation/components/search_filter_sheet.dart';
+import 'package:flutter_recipe_app_course/domain/filter/filter_state.dart';
 import 'package:flutter_recipe_app_course/presentation/search/screen/search_screen.dart';
 import 'package:flutter_recipe_app_course/presentation/search/search_view_model.dart';
 
@@ -16,6 +18,21 @@ class SearchRoot extends StatelessWidget {
         return SearchScreen(
           state: viewModel.state,
           onChanged: viewModel.searchRecipes,
+          onTapFilter: () {
+            showModalBottomSheet(
+              context: context,
+              isScrollControlled: true,
+              builder: (context) {
+                return SearchFilterSheet(
+                  state: viewModel.state.filterState,
+                  onChangeFilter: (FilterState state) {
+                    viewModel.onChangeFilter(state);
+                    Navigator.pop(context);
+                  },
+                );
+              },
+            );
+          },
         );
       },
     );
