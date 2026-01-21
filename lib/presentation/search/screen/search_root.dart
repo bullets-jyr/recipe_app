81e3a1b3e62fe4cffb2a153a608d2b4732341a2a
bullets-jyr <jy.ryu.jiyoung@gmail.com>
2026-01-21 13:41:29 +0900

    feat: Add recipe search feature

--- lib/presentation/search/screen/search_root.dart
+++ lib/presentation/search/screen/search_root.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_recipe_app_course/data/data_source/local/local_recent_search_recipe_data_source_impl.dart';
+import 'package:flutter_recipe_app_course/data/data_source/remote/remote_recipe_data_source_impl.dart';
 import 'package:flutter_recipe_app_course/data/repository/mock_recent_search_recipe_repository_impl.dart';
+import 'package:flutter_recipe_app_course/data/repository/mock_recipe_repository_impl.dart';
+import 'package:flutter_recipe_app_course/domain/use_case/search_recipes_use_case.dart';
 import 'package:flutter_recipe_app_course/presentation/search/screen/search_screen.dart';
 import 'package:flutter_recipe_app_course/presentation/search/search_view_model.dart';
 
@@ -15,12 +18,24 @@ class SearchRoot extends StatelessWidget {
   Widget build(BuildContext context) {
     final viewModel = SearchViewModel(
       recentSearchRecipeRepository: _recentSearchRecipeRepository,
+      searchRecipesUseCase: SearchRecipesUseCase(
+        recipeRepository: MockRecipeRepositoryImpl(
+          recipeDataSource: RemoteRecipeDataSourceImpl(),
+        ),
+      ),
     );
 
     return ListenableBuilder(
       listenable: viewModel,
       builder: (context, widget) {
-        return SearchScreen(state: viewModel.state);
+        return SearchScreen(
+          state: viewModel.state,
+          // onChanged: (query) {
+          //   viewModel.searchRecipes(query);
+          // },
+          // onChanged: (query) => viewModel.searchRecipes(query),
+          onChanged: viewModel.searchRecipes,
+        );
       },
     );
   }
